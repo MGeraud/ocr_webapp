69c50428a800250fefe1dce7e4e18f0740aa181a
MGeraud <geraud.mon@gmail.com>
2020-11-04 13:40:57 +0100

    Refactor index controller to use RestTemplate instead of Feign

--- src/main/java/com/geraud/ocr_webapp/controllers/IndexController.java
+++ src/main/java/com/geraud/ocr_webapp/controllers/IndexController.java
@@ -1,27 +1,24 @@
 package com.geraud.ocr_webapp.controllers;
 
 import com.geraud.ocr_webapp.model.Book;
-import com.geraud.ocr_webapp.proxies.OcrBibliothequeProxy;
 import com.geraud.ocr_webapp.utils.Login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 
 @Controller
 public class IndexController {
 
     @Autowired
-    OcrBibliothequeProxy ocrBibliothequeProxy;
-
+    RestTemplate restTemplate;
     @RequestMapping({"" , "/" , "/index" ,"/index.html"})
-    public String index(Model model , Integer page){
+    public String index(Model model ){
 
-        if (page == null ){page = 0;};
-        PagedModel<EntityModel<Book>> books = ocrBibliothequeProxy.showAllWithPagination(page);
+        EntityModel<Book> books = restTemplate.getForObject("http://localhost:9090/books/" , EntityModel.class);
         model.addAttribute("books" , books);
         model.addAttribute("identifiants", new Login());
 
