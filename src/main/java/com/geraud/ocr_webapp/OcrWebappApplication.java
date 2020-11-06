9b5739b3e4b6533b71bb36b6e5278eb506498849
MGeraud <geraud.mon@gmail.com>
2020-11-06 17:08:53 +0100

    Add patch feature to extend loan period

--- src/main/java/com/geraud/ocr_webapp/OcrWebappApplication.java
+++ src/main/java/com/geraud/ocr_webapp/OcrWebappApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableFeignClients
 public class OcrWebappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/geraud/ocr_webapp/controllers/LoanController.java
+++ src/main/java/com/geraud/ocr_webapp/controllers/LoanController.java
@@ -4,16 +4,12 @@ import com.geraud.ocr_webapp.model.Loan;
 import com.geraud.ocr_webapp.utils.Login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 
 @Controller
 public class LoanController {
@@ -39,5 +35,21 @@ public class LoanController {
         model.addAttribute("myLoans" , myLoans);
 
         return "listMyLoans";
+    }
+
+    @RequestMapping("/loan/{id}/extend")
+    public String extendLoan(@PathVariable(value = "id") Long id){
+
+        Loan loan = new Loan();
+        loan.setId(id);
+        loan.setRefreshEndingCounter(1);
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        restTemplate.setRequestFactory(requestFactory);
+
+        //création de l'url à appeler à partir des critères de recherche récupérés (email et carte membre)
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:9092/loan/" + loan.getId())
+                .toUriString();
+        restTemplate.patchForObject(url,loan , Loan.class);
+       return "redirect:/index";
     }
 }
